280789df825ac3300cf90db47ae27090aa28d694
Karol Czernuszka <kczernuszka@o2.pl>
2020-04-13 16:21:47 +0200

    Addition numbers

--- hello.py
+++ hello.py
@@ -7,4 +7,12 @@ def read_numbers_from_string(string):
         return numbers
     return 0
 
+def add(string):
+    numbers = read_numbers_from_string(string)
+    sum = 0
+    if numbers:
+        for number in numbers:
+            sum += number
+    return sum
+
 
--- unittests.py
+++ unittests.py
@@ -1,4 +1,4 @@
-from hello import read_numbers_from_string
+from hello import read_numbers_from_string, add
 import unittest
 
 class Test_TestAddition(unittest.TestCase):
@@ -9,6 +9,10 @@ class Test_TestAddition(unittest.TestCase):
     def test_read_numbers_from_string(self):
         self.assertEqual(read_numbers_from_string("1"), [1])
         self.assertEqual(read_numbers_from_string("1,2,3"), [1,2,3])
+    
+    def test_add_numbers(self):
+        self.assertEqual(add(""), 0)
+        self.assertEqual(add("4,5,2,7"), 18)
 
 if __name__ == '__main__':
     unittest.main()       
