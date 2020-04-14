5cc09b5bd61210a578b8064a43727a5f566c65a0
Karol Czernuszka <kczernuszka@o2.pl>
2020-04-14 18:00:16 +0200

    Convert to object-oriented paradigm

--- hello.py
+++ hello.py
@@ -3,67 +3,69 @@ import re
 DEFAULT_DELIMITER = ','
 NEW_LINE_CHARACTER = '\n'
 
-def read_numbers_from_string(string):
-    if string:
-        first_token = string.split('\n')[0]
-        if is_change_delimiter_string(first_token):
-            delimiter = read_delimiter(first_token)
-            string = string.split('\n', 1)[1]
-            if delimiter == '':
-                raise Exception('Wrong first line format - ignored.')
+class Calculator:
+
+    def add(self, string):
+        numbers = self.__read_numbers_from_string(string)
+        sum = 0
+        if numbers:
+            for number in numbers:
+                sum += number
+        return sum
+
+    def multiply(self, string):
+        numbers = self.__read_numbers_from_string(string)
+        mul = 1
+        if numbers:
+            for number in numbers:
+                mul *= number
+            return mul
+        else:
+            return 0       
+
+    def __read_numbers_from_string(self, string):
+        if string:
+            first_token = string.split('\n')[0]
+            if self.__is_change_delimiter_string(first_token):
+                delimiter = self.__read_delimiter(first_token)
+                string = string.split('\n', 1)[1]
+                if delimiter == '':
+                    raise Exception('Wrong first line format - ignored.')
+                    delimiter = DEFAULT_DELIMITER
+            else:
                 delimiter = DEFAULT_DELIMITER
-        else:
-            delimiter = DEFAULT_DELIMITER
-        tokens = re.split('[{}{}]'.format(delimiter, NEW_LINE_CHARACTER), string)
-        if tokens_are_numbers(tokens):
-            numbers = list(map(int, tokens))
-            return numbers
-        else:
-            raise Exception('Unknown string format:{}'.format(string))
-    return 0
+            tokens = re.split('[{}{}]'.format(delimiter, NEW_LINE_CHARACTER), string)
+            if self.__tokens_are_numbers(tokens):
+                numbers = list(map(int, tokens))
+                return numbers
+            else:
+                raise Exception('Unknown string format:{}'.format(string))
+        return 0
 
-def add(string):
-    numbers = read_numbers_from_string(string)
-    sum = 0
-    if numbers:
-        for number in numbers:
-            sum += number
-    return sum
-
-def tokens_are_numbers(tokens):
-    for token in tokens:
-        if not token_is_number(token):
+    def __is_change_delimiter_string(self, string):
+        token = string.split('\n')[0]
+        if token.startswith('\\'):
+            return 1
+        else:
             return 0
-    return 1
 
-def token_is_number(token):
-    try:
-        int(token)
-        is_dig = 1
-    except ValueError:
-        is_dig = 0
-    return is_dig
+    def __read_delimiter(self, delimiter_string):
+        if len(delimiter_string) >= 2:
+            delimiter = delimiter_string[1]
+            return delimiter
+        else:
+            return ''
 
-def read_delimiter(delimiter_string):
-    if len(delimiter_string) >= 2:
-        delimiter = delimiter_string[1]
-        return delimiter
-    else:
-        return ''
-
-def is_change_delimiter_string(string):
-    token = string.split('\n')[0]
-    if token.startswith('\\'):
+    def __tokens_are_numbers(self, tokens):
+        for token in tokens:
+            if not self.__token_is_number(token):
+                return 0
         return 1
-    else:
-        return 0
 
-def multiply(string):
-    numbers = read_numbers_from_string(string)
-    mul = 1
-    if numbers:
-        for number in numbers:
-            mul *= number
-        return mul
-    else:
-        return 0
+    def __token_is_number(self, token):
+        try:
+            int(token)
+            is_dig = 1
+        except ValueError:
+            is_dig = 0
+        return is_dig
--- unittests.py
+++ unittests.py
@@ -1,47 +1,36 @@
-from hello import read_numbers_from_string, add, multiply, read_delimiter
+from hello import Calculator
 import unittest
 
 class Test_TestAddition(unittest.TestCase):
 
-    def test_read_numbers_from_empty_string(self):
-        self.assertFalse(read_numbers_from_string(""))
-
-    def test_read_numbers_from_string(self):
-        self.assertEqual(read_numbers_from_string("1"), [1])
-        self.assertEqual(read_numbers_from_string("1,2,3"), [1,2,3])
-
-    def test_read_numbers_from_string_with_new_lines(self):
-        self.assertEqual(read_numbers_from_string("1\n2\n3"), [1,2,3])
-        self.assertEqual(read_numbers_from_string("1\n2,3"), [1,2,3])
-
-    def test_read_negative_numbers_from_string(self):
-        self.assertEqual(read_numbers_from_string("1,-2,-3"), [1,-2,-3])
+    def setUp(self):
+        self.calculator = Calculator()
     
     def test_validate_tokens(self):
-        self.assertRaises(Exception, read_numbers_from_string, "g,y,c,u")
-        self.assertRaises(Exception, read_numbers_from_string, "wrongString")
-        self.assertRaises(Exception, read_numbers_from_string, "1,2,g,3,7,8")
-        self.assertRaises(Exception, read_numbers_from_string, "1,\n2,3,3,7,8")
-        self.assertRaises(Exception, read_numbers_from_string, "\\;\n1,2,3")
-    
-    def test_read_delimiter(self):
-        self.assertEqual(read_delimiter('\\;\n1;2;3'), ';')
-        self.assertEqual(read_delimiter('\\-\n1\n2\n3'), '-')
+        self.assertRaises(Exception, self.calculator.add, "wrongString")
+        self.assertRaises(Exception, self.calculator.add, "1,2,g,3,7,8")
+        self.assertRaises(Exception, self.calculator.add, "1,\n2,3,3,7,8")
+        self.assertRaises(Exception, self.calculator.add, "\\;\n1,2,3")
 
-    def test_read_numbers_from_string_with_changing_delimiter(self):
-        self.assertEqual(read_numbers_from_string("\\;\n1;2;3"), [1,2,3])
-        self.assertEqual(read_numbers_from_string("\\;\n1\n2\n3"), [1,2,3])
-        self.assertEqual(read_numbers_from_string("\\;\n1;2\n3"), [1,2,3])
+    def test_calculation_from_empty_string(self):
+        self.assertEqual(self.calculator.add(""), 0)
+        self.assertEqual(self.calculator.multiply(""), 0)
 
-    def test_add_numbers(self):
-        self.assertEqual(add(""), 0)
-        self.assertEqual(add("4,5,2,7"), 18)
-        self.assertEqual(add("1,-2,-3"), -4)
-    
-    def test_multiply(self):
-        self.assertEqual(multiply(""), 0)
-        self.assertEqual(multiply("3,2,4"), 24)
-        self.assertEqual(multiply("3,-2,4"), -24)
+    def test_calculation_from_string(self):
+        self.assertEqual(self.calculator.add("3,9,15"), 27)
+        self.assertEqual(self.calculator.multiply("3,9,15"), 405)
+
+    def test_calculation_from_string_with_new_lines(self):
+        self.assertEqual(self.calculator.add("3\n9\n15"), 27)
+        self.assertEqual(self.calculator.add("3\n9,15"), 27)
+
+    def test_calculation_from__string_with_negative_numbers(self):
+        self.assertEqual(self.calculator.add("3,-9,15"), 9)
+
+    def test_calculation_from_string_with_changing_delimiter(self):
+        self.assertEqual(self.calculator.add("\\;\n3;9;15"), 27)
+        self.assertEqual(self.calculator.add("\\;\n3;9\n15"), 27)
+        self.assertEqual(self.calculator.add("\\;\n3\n9\n15"), 27)
 
 if __name__ == '__main__':
     unittest.main()       
